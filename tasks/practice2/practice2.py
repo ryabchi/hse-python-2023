b7022cc17bf2babc13c8a88ff2e06039bafd9519
Daniil Solynin <didhat82@gmail.com>
2023-04-23 20:21:04 +0300

    solve second practice

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,6 +1,27 @@
-from typing import Iterable
+import datetime
+import random
+import re
+from string import Template
+from typing import Iterable, Final
+from dataclasses import dataclass
 
-UNCULTURED_WORDS = ('kotleta', 'pirog')
+UNCULTURED_WORDS = ("kotleta", "pirog")
+
+
+common_greeting = Template("Привет, друг, $name")
+credit_order = Template(
+    "Фамилия: $surname\n"
+    "Имя: $name\n"
+    "Отчество: $patronymic\n"
+    "Дата рождения: $birthday\n"
+    "Запрошенная сумма: $amount"
+)
+
+
+min_amount_number: Final = 100
+max_amount_number: Final = 1000000
+
+ru_phone_number_validator = re.compile("\\+7\\d{10}")
 
 
 def greet_user(name: str) -> str:
@@ -11,8 +32,7 @@ def greet_user(name: str) -> str:
     :param name: имя пользователя
     :return: приветствие
     """
-
-    # пиши код здесь
+    greeting = common_greeting.substitute(name=name)
     return greeting
 
 
@@ -28,7 +48,8 @@ def get_amount() -> float:
     :return: случайную сумму на счете
     """
 
-    # пиши код здесь
+    amount = round(random.uniform(min_amount_number, max_amount_number), 2)
+
     return amount
 
 
@@ -41,7 +62,9 @@ def is_phone_correct(phone_number: str) -> bool:
     :return: буленовское значение - bool: True - если номер корректны,
                                           False - если номер некорректный
     """
-
+    result = (
+        True if ru_phone_number_validator.match(phone_number) is not None else False
+    )
     # пиши код здесь
     return result
 
@@ -58,8 +81,7 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
                                           False - если денег недостаточно
     """
 
-    # пиши код здесь
-    return result
+    return current_amount >= float(transfer_amount)
 
 
 def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
@@ -76,8 +98,11 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     :param uncultured_words: список запрещенных слов
     :return: текст, соответсвующий правилам
     """
+    result = text.strip().lower().capitalize().replace("'", "").replace('"', "")
 
-    # пиши код здесь
+    for word in uncultured_words:
+        result = result.replace(word, "#" * len(word))
+
     return result
 
 
@@ -85,20 +110,52 @@ def create_request_for_loan(user_info: str) -> str:
     """
     Генерирует заявку на кредит на основе входящей строки.
     Формат входящий строки:
-    
+
     Иванов,Петр,Сергеевич,01.01.1991,10000
-    
+
     Что должны вернуть на ее основе:
-    
+
     Фамилия: Иванов
     Имя: Петр
     Отчество: Сергеевич
     Дата рождения: 01.01.1991
     Запрошенная сумма: 10000
-    
+
     :param user_info: строка с информацией о клиенте
     :return: текст кредитной заявки
     """
 
-    # пиши код здесь
+    order = parse_pure_order_string(user_info)
+    result = order.to_text()
+
     return result
+
+
+@dataclass
+class CreditOrder:
+    name: str
+    surname: str
+    patronymic: str
+    birthday: datetime.datetime
+    credit_amount: int
+
+    def to_text(self):
+        return credit_order.substitute(
+            name=self.name,
+            surname=self.surname,
+            patronymic=self.patronymic,
+            birthday=self.birthday.strftime("%d.%m.%Y"),
+            amount=str(self.credit_amount),
+        )
+
+
+def parse_pure_order_string(user_info: str) -> CreditOrder:
+    parsed = user_info.split(",")
+
+    return CreditOrder(
+        name=parsed[1],
+        surname=parsed[0],
+        patronymic=parsed[2],
+        birthday=datetime.datetime.strptime(parsed[3], "%d.%m.%Y"),
+        credit_amount=int(parsed[4]),
+    )
