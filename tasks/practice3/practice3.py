53ec295b6fde9f519c987fd256374a9fe144b6df
Graf von Tee <grafvontee@yandex.ru>
2023-04-24 13:44:44 +0300

    Practice 3

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -1,3 +1,5 @@
+import re
+import csv
 from pathlib import Path
 from typing import Dict, Any, List, Optional
 
@@ -26,9 +28,10 @@ def count_words(text: str) -> Dict[str, int]:
              значение - количество вхождений слов в текст
     """
 
-    # пиши свой код здесь
+    text = re.sub(r"[-.?,:;()+!]", " ", text).lower().split()
+    resulted_dict = {word: text.count(word) for word in text if word.isalpha() and len(word) > 1}
 
-    return {}
+    return resulted_dict
 
 
 def exp_list(numbers: List[int], exp: int) -> List[int]:
@@ -40,9 +43,7 @@ def exp_list(numbers: List[int], exp: int) -> List[int]:
     :return: список натуральных чисел
     """
 
-    # пиши свой код здесь
-
-    return []
+    return [num ** exp for num in numbers]
 
 
 def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) -> float:
@@ -58,7 +59,8 @@ def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) 
     :return: размер кешбека
     """
 
-    return result
+    return sum(op['amount'] * (0.05 if op['category'] in special_category else 0.01)
+               for op in operations)
 
 
 def get_path_to_file() -> Optional[Path]:
@@ -99,6 +101,9 @@ def csv_reader(header: str) -> int:
     :return: количество уникальных элементов в столбце
     """
 
-    # пиши свой код здесь
+    with open(get_path_to_file(), 'r', newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        column_index = next(reader).index(header)
+        unique_values = set(row[column_index] for row in reader)
 
-    return 0
+    return len(unique_values)
