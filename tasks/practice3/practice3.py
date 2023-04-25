4dfb3d2cda7c54887540f9b0a833892e058d7b39
Chlorine355 <akimoff.an@gmail.com>
2023-04-25 19:21:23 +0300

    Practice 1-3
    Сделаны первые 3 практики

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -27,8 +27,16 @@ def count_words(text: str) -> Dict[str, int]:
     """
 
     # пиши свой код здесь
-
-    return {}
+    text = text.replace(",", " ").replace(".", " ")\
+        .replace("-", " ").replace("!", " ").replace("!", " ").replace(":", "")
+    text = [word.lower() for word in text.split() if word.isalpha()]
+    result = {}
+    for word in text:
+        if word in result:
+            result[word] += 1
+        else:
+            result[word] = 1
+    return result
 
 
 def exp_list(numbers: List[int], exp: int) -> List[int]:
@@ -40,9 +48,9 @@ def exp_list(numbers: List[int], exp: int) -> List[int]:
     :return: список натуральных чисел
     """
 
-    # пиши свой код здесь
+    numbers = [int(pow(x, exp)) for x in numbers]
 
-    return []
+    return numbers
 
 
 def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) -> float:
@@ -57,6 +65,12 @@ def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) 
     :param special_category: список категорий повышенного кешбека
     :return: размер кешбека
     """
+    result = 0
+    for operation in operations:
+        if operation["category"] in special_category:
+            result += operation["amount"] * 0.05
+        else:
+            result += operation["amount"] * 0.01
 
     return result
 
@@ -98,7 +112,8 @@ def csv_reader(header: str) -> int:
     :param header: название заголовка
     :return: количество уникальных элементов в столбце
     """
-
-    # пиши свой код здесь
-
-    return 0
+    import csv
+    with open(get_path_to_file(), newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        count = len(set([row[header] for row in reader]))
+    return count
