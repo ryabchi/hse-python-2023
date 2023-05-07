b6ac53331afeda44303fb5e2e3817a9ecb7775fc
pestrikv <pestrikv1337@gmail.com>
2023-05-07 17:09:06 +0300

    Practice 3

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -1,4 +1,6 @@
+import csv
 from pathlib import Path
+from string import punctuation
 from typing import Dict, Any, List, Optional
 
 
@@ -27,8 +29,16 @@ def count_words(text: str) -> Dict[str, int]:
     """
 
     # пиши свой код здесь
+    text_filter = text.strip().lower()
+    for char in punctuation:
+        text_filter = text_filter.replace(char, '')
 
-    return {}
+    result = {}
+    for word in text_filter.split():
+        if word.isalpha() and len(word) > 1:
+            result[word] = result.get(word, 0) + 1
+
+    return result
 
 
 def exp_list(numbers: List[int], exp: int) -> List[int]:
@@ -41,8 +51,7 @@ def exp_list(numbers: List[int], exp: int) -> List[int]:
     """
 
     # пиши свой код здесь
-
-    return []
+    return [x ** exp for x in numbers]
 
 
 def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) -> float:
@@ -57,6 +66,13 @@ def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) 
     :param special_category: список категорий повышенного кешбека
     :return: размер кешбека
     """
+    result = 0.0
+
+    for operation in operations:
+        if operation['category'] in special_category:
+            result += operation['amount'] * 0.05
+        else:
+            result += operation['amount'] * 0.01
 
     return result
 
@@ -100,5 +116,7 @@ def csv_reader(header: str) -> int:
     """
 
     # пиши свой код здесь
-
-    return 0
+    with open(get_path_to_file()) as csvfile:
+        lines = list(csv.reader(csvfile, delimiter=","))
+        header_index = lines[0].index(header)
+    return len({row[header_index] for row in lines[1:]})
