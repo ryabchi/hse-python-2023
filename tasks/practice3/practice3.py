5b3c89e4a6b08ca04cdbf2c60051d968dc9c286b
LaggyMan <45729776+LaggyMan@users.noreply.github.com>
2023-05-15 16:55:14 +0300

    Update practice3.py

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -102,7 +102,7 @@ def csv_reader(header: str) -> int:
     """
 
     file_csv = open(get_path_to_file(), 'r', newline='')
-    pos = csv.reader()
+    pos = csv.reader(file_csv)
     i_column = next(pos).index()
     uniq_elements = set(line[i_column] for line in pos)
     result = len(uniq_elements)
