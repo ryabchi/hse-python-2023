a609424e9d10224f12e5a493cbe49aa932cb1773
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-05-31 22:33:50 +0300

    practice 5 - second part

--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -28,6 +28,11 @@ class Team:
         """
 
         # пиши свой код здесь
+        if not isinstance(name, str) or not isinstance(manager, Manager):
+            raise ValueError
+        self.name = name
+        self.manager = manager
+        self.__members = set()
 
     def add_member(self, member: Employee) -> None:
         """
@@ -36,6 +41,9 @@ class Team:
         """
 
         # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError
+        self.__members.add(member)
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -44,6 +52,12 @@ class Team:
         """
 
         # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError
+        try:
+            self.__members.remove(member)
+        except Exception:
+            raise NoSuchMemberError(self.name, member)
 
     def get_members(self) -> Set[Employee]:
         """
@@ -52,6 +66,10 @@ class Team:
         """
 
         # пиши свой код здесь
+        return self.__members.copy()
+
+    def __str__(self):
+        return f'team: {self.name} manager: {self.manager.name} number of members: {len(self.__members)}'
 
     def show(self) -> None:
         """
