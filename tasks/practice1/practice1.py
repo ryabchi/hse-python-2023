397923d71ef5299dba1e95b5ea50d8fd144da204
DanilTs2003 <87439527+DanilTs2003@users.noreply.github.com>
2023-06-03 16:35:38 +0300

    Update practice1.py

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -2,14 +2,11 @@ def concatenate_strings(a: str, b: str) -> str:
     """
     Функция для сложения двух строк.
     Результат сложения запишите в переменную result.
-
     :param a: число
     :param b: число
     :return: результат сложения
     """
-
-    # пиши свой код здесь
-
+    result=a+b
     return result
 
 
@@ -17,11 +14,8 @@ def calculate_salary(total_compensation: int) -> float:
     """
     Функция расчета зарплаты, которую сотрудник получит после
     вычета налогов. Ставка налогообложения равна 13%.
-
     :param total_compensation: сумма зарплаты до вычета налога
     :return: сумма заплаты после вычета налога
     """
-
-    # пиши свой код здесь
-
-    return result
+    return total_compensation*0.87 
+print(concatenate_strings("sad","asdad"))
