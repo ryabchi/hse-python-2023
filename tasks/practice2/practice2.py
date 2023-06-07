1a76c89e2eb0eb4a47b5ecaf00d1f23d3e13c9eb
Sveboo <98907144+Sveboo@users.noreply.github.com>
2023-06-08 01:36:32 +0300

    practice_2

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,4 +1,6 @@
+import random
 from typing import Iterable
+import re
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -11,8 +13,7 @@ def greet_user(name: str) -> str:
     :param name: имя пользователя
     :return: приветствие
     """
-
-    # пиши код здесь
+    greeting = f"Hello, {name}"
     return greeting
 
 
@@ -28,7 +29,7 @@ def get_amount() -> float:
     :return: случайную сумму на счете
     """
 
-    # пиши код здесь
+    amount = round(random.random() * 1000000, 2)
     return amount
 
 
@@ -42,7 +43,7 @@ def is_phone_correct(phone_number: str) -> bool:
                                           False - если номер некорректный
     """
 
-    # пиши код здесь
+    result = True if re.match(r'^\+7\d{10}$', phone_number) else False
     return result
 
 
@@ -58,7 +59,7 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
                                           False - если денег недостаточно
     """
 
-    # пиши код здесь
+    result = current_amount >= float(transfer_amount)
     return result
 
 
@@ -77,7 +78,10 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     :return: текст, соответсвующий правилам
     """
 
-    # пиши код здесь
+    result = re.sub(r"\s+", " ", text.replace("\'", "").replace("\"", "").strip().capitalize())
+    for word in uncultured_words:
+        result = result.replace(word, "#" * len(word))
+
     return result
 
 
@@ -87,7 +91,7 @@ def create_request_for_loan(user_info: str) -> str:
     Формат входящий строки:
     
     Иванов,Петр,Сергеевич,01.01.1991,10000
-    
+
     Что должны вернуть на ее основе:
     
     Фамилия: Иванов
@@ -100,5 +104,10 @@ def create_request_for_loan(user_info: str) -> str:
     :return: текст кредитной заявки
     """
 
-    # пиши код здесь
+    info = user_info.split(',')
+    result = f'Фамилия: {info[0]}\n' \
+             f'Имя: {info[1]}\n' \
+             f'Отчество: {info[2]}\n' \
+             f'Дата рождения: {info[3]}\n' \
+             f'Запрошенная сумма: {info[4]}'
     return result
