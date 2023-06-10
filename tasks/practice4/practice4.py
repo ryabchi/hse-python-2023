3c30b0d2dbcb05e9e3842a15f5d47042fe10e8f6
LaggyMan <45729776+LaggyMan@users.noreply.github.com>
2023-06-10 22:33:12 +0300

    Practice 4

--- tasks/practice4/practice4.py
+++ tasks/practice4/practice4.py
@@ -37,7 +37,19 @@ def search_phone(content: Any, name: str) -> Optional[str]:
     :param name: имя пользователя, у которого будем искать номер телефона
     :return: номер телефона пользователя или None
     """
+    result = None
 
-    # пиши свой код здесь
-
-    return None
+    if isinstance(content,dict):
+        if 'name' in content and content.get('name') == name:
+            return content.get('phone')
+        else:
+            for x in content.values():
+                result = search_phone(x, name)
+                if result:
+                    return result
+    elif isinstance(content,list):
+        for x in content:
+            result = search_phone(x, name)
+            if result:
+                return result
+    else: return None
