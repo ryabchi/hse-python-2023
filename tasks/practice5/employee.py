b489b7b332e0bf6ea28c2fd61e89e887a9368e90
Dmitry Kutuzov <archie-woc@ya.ru>
2023-06-10 18:00:02 +0300

    practice 4 and 5

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -1,5 +1,4 @@
 from typing import Dict
-
 from .exception import NoSuchPositionError
 
 POSITIONS: Dict[str, int] = {
@@ -11,90 +10,57 @@ POSITIONS: Dict[str, int] = {
 
 
 def get_position_level(position_name: str) -> int:
-    """
-    Функция возвращает уровень позиции по ее названию. 
-    Если должности нет в базе поднимается исключение `NoSuchPositionError(position_name)`
-    """
     try:
         return POSITIONS[position_name]
-    except KeyError as exp:
-        raise NoSuchPositionError(position_name) from exp
+    except KeyError:
+        raise NoSuchPositionError(position_name)
 
 
 class Employee:
-    """
-    Класс - сотрудник
-
-    Возможности:
-    1. Реализована возможность сравнения двух сотрудников в зависимости от занимаемой должности - метод __eq__
-    2. Возможность получить зарплату через метод get_salary
-    """
     name: str
     position: str
     _salary: int
 
     def __init__(self, name: str, position: str, salary: int):
-        """
-        Задача: реализовать конструктор класса, чтобы все тесты проходили
-        """
-
-        # пиши свой код здесь
+        if type(name) is not str:
+            raise ValueError()
+        if type(position) is not str:
+            raise ValueError()
+        if type(salary) is not int:
+            raise ValueError()
+        self.name = name
+        self.position = position
+        self._salary = salary
 
     def get_salary(self) -> int:
-        """
-        Метод возвращает зарплату сотрудника.
-        """
-
-        # пиши свой код здесь
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
-        """
-        Задача: реализовать метод сравнение двух сотрудников, чтобы все тесты проходили.
-
-        Сравнение происходит по уровню позиции см. `get_position_level`.
-        Если что-то идет не так - бросаются исключения. Смотрим что происходит в тестах.
-        """
-
-        # пиши свой код здесь
+        if not isinstance(other, Employee):
+            raise TypeError()
+        try:
+            return get_position_level(self.position) == get_position_level(other.position)
+        except NoSuchPositionError as e:
+            raise ValueError(f"position ${e.position} does not exist")
 
     def __str__(self):
-        """
-        Задача: реализовать строковое представление объекта.
-        Пример вывода: 'name: Ivan position manager'
-        """
-
-        # пиши свой код здесь
+        return f'name: {self.name} position: {self.position}'
 
     def __hash__(self):
         return id(self)
 
 
 class Developer(Employee):
-    """
-    Сотрудник - разработчик
-    """
-
     language: str
     position: str = 'developer'
 
     def __init__(self, name: str, salary: int, language: str):
-        """
-        Задача: реализовать конструктор класса, используя конструктор родителя
-        """
-
-        # пиши свой код здесь
+        super().__init__(name, self.position, salary)
+        self.language = language
 
 
 class Manager(Employee):
-    """
-    Сотрудник - менеджер
-    """
-
     position: str = 'manager'
 
     def __init__(self, name: str, salary: int):
-        """
-        Задача: реализовать конструктор класса, используя конструктор родителя
-        """
-
-        # пиши свой код здесь
+        super().__init__(name, self.position, salary)
