d415c9e4f7da21ae4e3f75b46aaaf5dd186191dd
AEV26 <ast_egor@mail.ru>
2023-06-10 08:40:47 +0300

    practice2

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+import random
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -13,6 +14,7 @@ def greet_user(name: str) -> str:
     """
 
     # пиши код здесь
+    greeting = f'Hi, {name}!'
     return greeting
 
 
@@ -29,6 +31,7 @@ def get_amount() -> float:
     """
 
     # пиши код здесь
+    amount = round(random.uniform(100, 1000000), 2)
     return amount
 
 
@@ -43,6 +46,8 @@ def is_phone_correct(phone_number: str) -> bool:
     """
 
     # пиши код здесь
+    result = (len(phone_number) == 12 and phone_number[0] == '+'and phone_number[1] == '7'
+              and all(s.isdigit() for s in phone_number[2:]))
     return result
 
 
@@ -59,6 +64,7 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
     """
 
     # пиши код здесь
+    result = current_amount >= float(transfer_amount)
     return result
 
 
@@ -78,6 +84,17 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     """
 
     # пиши код здесь
+    result = text.replace('\'', '')
+    result = result.replace('\"', '')
+
+    result = result.split()
+    for i in range(len(result)):
+        for rude in uncultured_words:
+            if rude in result[i].lower():
+                result[i] = result[i].replace(rude, '#' * len(rude))
+
+    result = ' '.join(result)
+    result = result[0].upper() + result[1:].lower()
     return result
 
 
@@ -101,4 +118,7 @@ def create_request_for_loan(user_info: str) -> str:
     """
 
     # пиши код здесь
+    surname, name, patronymic, day_of_birth, money = user_info.split(',')
+    result = (f'Фамилия: {surname}\nИмя: {name}\nОтчество: {patronymic}\n'
+              f'Дата рождения: {day_of_birth}\nЗапрошенная сумма: {money}')
     return result
