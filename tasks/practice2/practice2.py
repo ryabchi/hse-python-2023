c8ba85f51f212a7800ed297217ca76ea187f6561
Elina Karapetian <elina.karapetyan.00@mail.ru>
2023-06-13 09:40:09 +0300

    second practice

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,4 +1,6 @@
 from typing import Iterable
+from random import uniform
+from re import fullmatch
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -13,7 +15,7 @@ def greet_user(name: str) -> str:
     """
 
     # пиши код здесь
-    return greeting
+    return f'Hey, {name}'
 
 
 def get_amount() -> float:
@@ -29,7 +31,7 @@ def get_amount() -> float:
     """
 
     # пиши код здесь
-    return amount
+    return round(uniform(100, 1000000), 2)
 
 
 def is_phone_correct(phone_number: str) -> bool:
@@ -43,7 +45,7 @@ def is_phone_correct(phone_number: str) -> bool:
     """
 
     # пиши код здесь
-    return result
+    return fullmatch(r'^\++?7\d{10}', phone_number) is not None
 
 
 def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
@@ -59,7 +61,7 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
     """
 
     # пиши код здесь
-    return result
+    return current_amount >= float(transfer_amount)
 
 
 def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
@@ -78,7 +80,11 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     """
 
     # пиши код здесь
-    return result
+    text = text.strip()
+    text = text[0].upper() + text[1:].lower().replace('\"', "").replace("\'", "")
+    for dangerous in uncultured_words:
+        text = text.replace(dangerous, "#" * len(dangerous))
+    return text
 
 
 def create_request_for_loan(user_info: str) -> str:
@@ -101,4 +107,4 @@ def create_request_for_loan(user_info: str) -> str:
     """
 
     # пиши код здесь
-    return result
+    return "Фамилия: {0}\nИмя: {1}\nОтчество: {2}\nДата рождения: {3}\nЗапрошенная сумма: {4}".format(*user_info.split(','))
