819d5319fcb3fb32b100390370c2a52ef6d4ac46
RennI <98748434+Renne103@users.noreply.github.com>
2023-06-13 11:25:43 +0300

    Update practice1.py

--- tasks/practice1/practice1.py
+++ tasks/practice1/practice1.py
@@ -7,7 +7,7 @@ def concatenate_strings(a: str, b: str) -> str:
     :param b: число
     :return: результат сложения
     """
-
+    # пиши свой код здесь
     result = a + b
 
     return result
@@ -21,7 +21,7 @@ def calculate_salary(total_compensation: int) -> float:
     :param total_compensation: сумма зарплаты до вычета налога
     :return: сумма заплаты после вычета налога
     """
-
+    # пиши свой код здесь
     result = total_compensation * 0.87
 
     return result
