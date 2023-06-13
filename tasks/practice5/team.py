16273ec82bfd04b096c502ce3c88e4ac2152ab6f
NewRonin <max.md8@gmail.com>
2023-06-13 04:15:29 +0300

    finally practice5

--- tasks/practice5/team.py
+++ tasks/practice5/team.py
@@ -1,7 +1,7 @@
 from typing import Set
 from .employee import Employee, Manager
 from .exception import NoSuchMemberError
-
+import copy
 
 class Team:
     """
@@ -28,14 +28,32 @@ class Team:
         """
 
         # пиши свой код здесь
+        self.name = name
+        self.manager = manager
+        self.__members = set()
+
+    def __str__(self):
+        """
+        Задача: реализовать строковое представление объекта.
+        Пример вывода: 'name: Ivan position manager'
+        """
+
+        # пиши свой код здесь
+        return "team: {team_name} manager: {manager_name} number of members: {members_count}".format(team_name = self.name, manager_name = self.manager.name, members_count = len(self.get_members()))
 
     def add_member(self, member: Employee) -> None:
         """
         Задача: реализовать метод добавления участника в команду.
         Добавить можно только работника.
         """
+        if not isinstance(member, Employee):
+            raise TypeError
 
         # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError
+
+        self.__members.add(member)
 
     def remove_member(self, member: Employee) -> None:
         """
@@ -44,6 +62,13 @@ class Team:
         """
 
         # пиши свой код здесь
+        if not isinstance(member, Employee):
+            raise TypeError
+
+        try:
+            self.__members.remove(member)
+        except KeyError:
+            raise NoSuchMemberError(self.name, member)
 
     def get_members(self) -> Set[Employee]:
         """
@@ -52,6 +77,7 @@ class Team:
         """
 
         # пиши свой код здесь
+        return copy.copy(self.__members)
 
     def show(self) -> None:
         """
