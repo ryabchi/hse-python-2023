46154279d1154035b0b392e6a6ad04df8acc0b35
2Ez4Joker <118948467+2Ez4Joker@users.noreply.github.com>
2023-06-13 21:51:36 +0300

    Update practice4.py

--- tasks/practice4/practice4.py
+++ tasks/practice4/practice4.py
@@ -39,5 +39,21 @@ def search_phone(content: Any, name: str) -> Optional[str]:
     """
 
     # пиши свой код здесь
+    number = None
+
+    if isinstance(content, dict):
+        if content.get('name', None) == name:
+            return content.get('phone', None)
+        for i in content.values():
+            number = search_phone(i, name)
+            if number is not None:
+                return number
+    elif isinstance(content, list):
+        for i in content:
+            number = search_phone(i, name)
+            if number is not None:
+                return number
+
+    return number
 
     return None
